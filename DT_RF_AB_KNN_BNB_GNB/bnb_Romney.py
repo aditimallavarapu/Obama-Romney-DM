16d8387aa1a9fd3ec967e96fe9c94744e5bf20fd
suganyasiva <sugu.sivakumar@gmail.com>
2017-03-29 15:20:45 -0500

    Added 10 fold cross validation
    Refer ReadMe_Suganya.txt for classification metrics for all the models

--- DT_RF_AB_KNN_BNB_GNB/bnb_Romney.py
+++ DT_RF_AB_KNN_BNB_GNB/bnb_Romney.py
@@ -18,27 +18,54 @@ word_features = analysis.get_word_features(analysis.get_words_in_tweets(tweets))
 
 test_tweets, test_tweetlist, test_labels = analysis.read_file("Romney_test_data_cleaned.txt",1)
 
-tfidf_vectorizer = TfidfVectorizer()
-#tfidf_vectorizer = CountVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(tweetlist)
-test_tfidf_matrix = tfidf_vectorizer.transform(test_tweetlist)
+number_of_folds = 10
+subset_size = len(tweetlist)/number_of_folds
+precisionlist0 = []
+precisionlist1 = []
+precisionlist_1 = []
+recalllist0 = []
+recalllist1 = []
+recalllist_1 = []
+f1scorelist0 = []
+f1scorelist1 = []
+f1scorelist_1 = []
+accuracylist = []
 
-cosine_similarities = cosine_similarity(test_tfidf_matrix, tfidf_matrix)
-predicted_labels = []
-top_predictions = []
+for i in range(number_of_folds):
+    test_data = tweetlist[i*subset_size:][:subset_size]
+    train_data = tweetlist[:i*subset_size] + tweetlist[(i+1)*subset_size:]
+    test_data_labels = labels[i*subset_size:][:subset_size]
+    train_data_labels = labels[:i*subset_size] + labels[(i+1)*subset_size:]
+    tfidf_vectorizer = TfidfVectorizer()
+#    tfidf_vectorizer = CountVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(train_data)
+    test_tfidf_matrix = tfidf_vectorizer.transform(test_data)
 
-bnb = BernoulliNB()
-bnb.fit(tfidf_matrix, labels)
-predicted_labels = bnb.predict(test_tfidf_matrix)
-    
-precision0, recall0, f1score0, class_accuracy0, overall_accuracy = analysis.metrics("0\n", predicted_labels, test_labels)
-precision1, recall1, f1score1, class_accuracy1, overall_accuracy = analysis.metrics("1\n", predicted_labels, test_labels)
-precision_1, recall_1, f1score_1, class_accuracy_1, overall_accuracy = analysis.metrics("-1\n", predicted_labels, test_labels)
-print("F1 Score - Class 0", f1score0)
-print("F1 Score - Class 1", f1score1)
-print("F1 Score - Class -1", f1score_1)
-print("Accuracy ", (class_accuracy0 + class_accuracy1 + class_accuracy_1) / 3)
+    cosine_similarities = cosine_similarity(test_tfidf_matrix, tfidf_matrix)
+    classifier = BernoulliNB().fit(tfidf_matrix, train_data_labels)
 
-
-
-
+    predictions = classifier.predict(test_tfidf_matrix)
+    precision0, recall0, f1score0, class_accuracy0, overall_accuracy = analysis.metrics("0\n", predictions, test_data_labels)
+    precision1, recall1, f1score1, class_accuracy1, overall_accuracy = analysis.metrics("1\n", predictions, test_data_labels)
+    precision_1, recall_1, f1score_1, class_accuracy_1, overall_accuracy = analysis.metrics("-1\n", predictions, test_data_labels)
+    precisionlist0.append(precision0)
+    precisionlist1.append(precision1)
+    precisionlist_1.append(precision_1)
+    recalllist0.append(recall0)
+    recalllist1.append(recall1)
+    recalllist_1.append(recall_1)
+    f1scorelist0.append(f1score0)
+    f1scorelist1.append(f1score1)
+    f1scorelist_1.append(f1score_1)
+    accuracylist.append((class_accuracy0 + class_accuracy1 + class_accuracy_1) / 3)
+print("Bernoulli Naive Bayes Classifier - Romney")
+print("Precision - Class 0", sum(precisionlist0) / float(len(precisionlist0)))
+print("Precision - Class 1", sum(precisionlist1) / float(len(precisionlist1)))
+print("Precision - Class -1", sum(precisionlist_1) / float(len(precisionlist_1)))
+print("Recall - Class 0", sum(recalllist0) / float(len(recalllist0)))
+print("Recall - Class 1", sum(recalllist1) / float(len(recalllist1)))
+print("Recall - Class -1", sum(recalllist_1) / float(len(recalllist_1)))
+print("F1 Score - Class 0", sum(f1scorelist0) / float(len(f1scorelist0)))
+print("F1 Score - Class 1", sum(f1scorelist1) / float(len(f1scorelist1)))
+print("F1 Score - Class -1", sum(f1scorelist_1) / float(len(f1scorelist_1)))
+print("Overall Accuracy ", sum(accuracylist) / float(len(accuracylist)))
