aadacb388c02d941058ad6482776a72b862b0117
suganyasiva <sugu.sivakumar@gmail.com>
2017-03-29 13:52:04 -0500

    Added 10 fold cross validation to MNB classifier
    Obama data:
    ('F1 Score - Class 0', 0.3464723147401066)
    ('F1 Score - Class 1', 0.5011377650448001)
    ('F1 Score - Class -1', 0.52703926989556)
    ('Accuracy ', 0.5112024500517143)
    
    Romney Data:
    ('F1 Score - Class 0', 0.2289193287368938)
    ('F1 Score - Class 1', 0.2724751496575107)
    ('F1 Score - Class -1', 0.6352558364072787)
    ('Accuracy ', 0.40320315206594903)

--- MultinomialNaiveBayes and CosineSimilarity/MultinomialNB_Romney.py
+++ MultinomialNaiveBayes and CosineSimilarity/MultinomialNB_Romney.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Mar 21 20:42:46 2017
 
-@author: Sugu
+@author: Suganya
 """
 
 from TwitterSentimentAnalysis import TwitterSentimentAnalysis
@@ -18,24 +18,38 @@ word_features = analysis.get_word_features(analysis.get_words_in_tweets(tweets))
 
 test_tweets, test_tweetlist, test_labels = analysis.read_file("Romney_test_data_cleaned.txt",1)
 
-#tfidf_vectorizer = TfidfVectorizer()
-tfidf_vectorizer = CountVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(tweetlist)
-test_tfidf_matrix = tfidf_vectorizer.transform(test_tweetlist)
+number_of_folds = 10
+subset_size = len(tweetlist)/number_of_folds
+f1scorelist0 = []
+f1scorelist1 = []
+f1scorelist_1 = []
+accuracylist = []
 
-cosine_similarities = cosine_similarity(test_tfidf_matrix, tfidf_matrix)
-predicted_labels = []
-top_predictions = []
+for i in range(number_of_folds):
+    test_data = tweetlist[i*subset_size:][:subset_size]
+    train_data = tweetlist[:i*subset_size] + tweetlist[(i+1)*subset_size:]
+    test_data_labels = labels[i*subset_size:][:subset_size]
+    train_data_labels = labels[:i*subset_size] + labels[(i+1)*subset_size:]
+    #tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer = CountVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(train_data)
+    test_tfidf_matrix = tfidf_vectorizer.transform(test_data)
 
-clf = MultinomialNB().fit(tfidf_matrix, labels)
+    cosine_similarities = cosine_similarity(test_tfidf_matrix, tfidf_matrix)
+    clf = MultinomialNB().fit(tfidf_matrix, train_data_labels)
 
-predictions = clf.predict(test_tfidf_matrix)
-precision0, recall0, f1score0, class_accuracy0, overall_accuracy = analysis.metrics("0\n", predictions, test_labels)
-precision1, recall1, f1score1, class_accuracy1, overall_accuracy = analysis.metrics("1\n", predictions, test_labels)
-precision_1, recall_1, f1score_1, class_accuracy_1, overall_accuracy = analysis.metrics("-1\n", predictions, test_labels)
-print("F1 Score - Class 0", f1score0)
-print("F1 Score - Class 1", f1score1)
-print("F1 Score - Class -1", f1score_1)
-print("Accuracy ", (class_accuracy0 + class_accuracy1 + class_accuracy_1) / 3)
+    predictions = clf.predict(test_tfidf_matrix)
+    precision0, recall0, f1score0, class_accuracy0, overall_accuracy = analysis.metrics("0\n", predictions, test_data_labels)
+    precision1, recall1, f1score1, class_accuracy1, overall_accuracy = analysis.metrics("1\n", predictions, test_data_labels)
+    precision_1, recall_1, f1score_1, class_accuracy_1, overall_accuracy = analysis.metrics("-1\n", predictions, test_data_labels)
+    f1scorelist0.append(f1score0)
+    f1scorelist1.append(f1score1)
+    f1scorelist_1.append(f1score_1)
+    accuracylist.append((class_accuracy0 + class_accuracy1 + class_accuracy_1) / 3)
+print("F1 Score - Class 0", sum(f1scorelist0) / float(len(f1scorelist0)))
+print("F1 Score - Class 1", sum(f1scorelist1) / float(len(f1scorelist1)))
+print("F1 Score - Class -1", sum(f1scorelist_1) / float(len(f1scorelist_1)))
+print("Accuracy ", sum(accuracylist) / float(len(accuracylist)))
+
 
 
